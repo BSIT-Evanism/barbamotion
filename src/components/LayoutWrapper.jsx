d9f658a2de2322bb04d79aa867f4a821b79be4ca
BSIT-Evanism <71175442+BSIT-Evanism@users.noreply.github.com>
2023-12-29 21:39:12 +0800

    refactor navbar

--- src/components/LayoutWrapper.jsx
+++ src/components/LayoutWrapper.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useStore } from '@nanostores/react'
 import { toggle } from '../globalStore'
+
 
 function LayoutWrapper({ children }) {
     const $toggle = useStore(toggle)
 
     return (
-        <div className={`${$toggle ? "scale-90 backdrop-brightness-70 overflow-hidden" : "scale-100 overflow-scroll"} transition-all duration-2000 border-2 ease-[cubic-bezier(0.81, 0.15, 0.06, 0.95)] h-screen w-full`}>
+        <div className={`${$toggle ? "scale-90 backdrop-brightness-70 overscroll-y-none overflow-hidden" : "scale-100 overflow-auto overscroll-y-auto"} pointer-events-none transition-all duration-2000 border-2 ease-[cubic-bezier(0.81, 0.15, 0.06, 0.95)] h-fit w-full`}>
             {children}
         </div>
     )
--- src/components/ToggleBottomBar.jsx
+++ src/components/ToggleBottomBar.jsx
@@ -2,29 +2,44 @@ import React, { useEffect, useState } from 'react'
 import { toggle, handleToggle, viewToggle } from '../globalStore'
 import { useStore } from '@nanostores/react'
 import { animate, spring } from 'motion'
-import { motion } from 'framer-motion'
+import { AnimatePresence, motion, useScroll, useSpring } from 'framer-motion'
 
 function ToggleBottomBar() {
     const $toggle = useStore(toggle)
     const $viewToggle = useStore(viewToggle)
     const [change, setChange] = useState('')
+    const [hover, setHover] = useState(false)
 
+    const { scrollYProgress } = useScroll()
+    const scaleX = useSpring(scrollYProgress, {
+        stiffness: 100,
+        damping: 30,
+        restDelta: 0.001
+    })
 
 
 
     useEffect(() => {
-        animate('#togglebutton', { width: $toggle ? "60vw" : "20vw", height: $toggle ? "40vh" : "90px", left: $toggle ? '20vw' : '40vw' }, { duration: 2, easing: [0.81, 0.15, 0.06, 0.95] })
-    }, [$toggle])
+        animate('#togglebutton', { width: $toggle ? "60vw" : "20vw", height: $toggle ? "40vh" : "100px" }, { duration: 2, easing: [0.81, 0.15, 0.06, 0.95] })
+        animate('#togglebutton', { left: $toggle ? '20vw' : '40vw' }, { duration: 2, easing: [0.81, 0.15, 0.06, 0.95] })
+    }, [$toggle, hover])
 
     return (
-        <motion.div animate={{ x: $viewToggle ? "100vw" : 0 }} transition={{ duration: 1, ease: [0.41, 0.15, 0.06, 0.95] }}>
-            <div id="togglebutton" className={`fixed bottom-20 border-2 ${$toggle ? "border-slate" : "border-black"} flex justify-around 'items-start' gap-4 px-10 py-7 rounded-8 z-5 bg-white font-italic text-xl transition-all duration-1000`} >
-                <div onClick={handleToggle} className="w-6 h-6 p-4 bg-black rounded-full">
-
-                </div>
-                <h1>About</h1>
-                <h1>Contact</h1>
-            </div>
+        <motion.div id="togglebutton" onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)} className="fixed bottom-20 border-2 rounded-10 p-[0.2rem] overflow-hidden" animate={{ x: $viewToggle ? "100vw" : 0 }} transition={{ duration: 1, ease: [0.41, 0.15, 0.06, 0.95] }}>
+            <motion.div style={{ scaleX }} className="absolute w-full h-full top-0 left-0 bg-[#22272C] -z-1"></motion.div>
+            <motion.div className={` ${$toggle ? "border-slate" : "border-black"} flex justify-around items-start h-full w-full gap-4 px-10 py-7 rounded-[2.3rem] z-5 bg-white font-italic text-xl transition-all duration-1000`} >
+                <AnimatePresence>
+                    <motion.div layoutId='button' onClick={handleToggle} className="w-6 h-6 p-4 cursor-pointer hover:opacity-70 bg-black rounded-full">
+                    </motion.div>
+                    {hover || $toggle ?
+                        (<>
+                            <motion.h1 initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>About</motion.h1>
+                            <motion.h1 initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>Contact</motion.h1>
+                        </>)
+                        : null
+                    }
+                </AnimatePresence>
+            </motion.div>
         </motion.div>
     )
 }

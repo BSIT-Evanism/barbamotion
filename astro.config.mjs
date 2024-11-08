e3b799dd88d8e4aa382d4da67b29f08426496ac8
BSIT-Evanism <johnevanporcioncula.solanoy@bicol-u.edu.ph>
2024-11-08 12:25:44 +0800

    qf11

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import sentry from "@sentry/astro";
 import spotlightjs from "@spotlightjs/astro";
-import UnoCSS from 'unocss/astro';
+import UnoCSS from "unocss/astro";
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel/serverless";
-
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [UnoCSS({
-    injectReset: true
-  }), react()],
+  integrations: [
+    UnoCSS({
+      injectReset: true,
+    }),
+    react(),
+  ],
   output: "static",
 });

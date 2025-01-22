71cdf90f386c25f830db2f520aef5f47999837d5
BSIT-Evanism <johnevanporcioncula.solanoy@bicol-u.edu.ph>
2025-01-22 17:33:46 +0800

    rm

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import UnoCSS from "unocss/astro";
 import react from "@astrojs/react";
 import node from "@astrojs/node";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
@@ -12,7 +14,5 @@ export default defineConfig({
     react(),
   ],
   output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: vercel(),
 });
--- dist/client/_astro/hoisted.O0XSsUBF.js
+++
@@ -1,1 +0,0 @@
-import"./hoisted.Co-Dy5Pv.js";import"./globalStore.CEGIqPQe.js";import"./stagger.es.D5VqDT9Q.js";
